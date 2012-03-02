df72633120fe6f223a8fb2622a8ba39b2cefb838
Jerry <hbjerryzl@gmail.com>
2012-03-02 20:55:37 +0800

    test

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -41,6 +41,8 @@
 // @require	https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // ==/UserScript== 
 
+
+
 function withjQuery(callback, safe){
 	if(typeof(jQuery) == "undefined") {
 		var script = document.createElement("script");
